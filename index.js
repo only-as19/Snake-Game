70f80c0e48bbac1970635736b807bead4487b7ba
Asad <hellohello51981@gmail.com>
2025-06-18 23:15:28 +0500

    Add JS -  add all the important variables that will used in future working

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const grid = document.querySelector(".grid");
+
+const scoreDisplay = document.getElementById("score");
+const startBtn = document.getElementById("start");
+
 let squares = [];
+let currentSnake = [2,1,0]
+let direction = 1;
+let width = 10;
+let appleIndex = 0;
+let score = 0;
+let intervelTime = 1000;
+let speed = 0.9;
+let timerId = 0;
 
 
 
@@ -12,3 +24,4 @@ for (let i = 0; i< 100; i++){
 
 }
 }
+createGrid();
